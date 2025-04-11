42dcb2d47dd97fd806c72ecda18148a99946008c
hirock0 <hirockdutta0@gmail.com>
2025-04-11 20:57:24 +0600

    some issues resolve]"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
     },
   },
 ];
